f08a4a30bbe3a2189e3479734a99ad61255ca406
Slavk <slavk11@yandex.ru>
2023-03-23 09:09:11 +0300

    Fixed logic of switch colors, added adaptive code for different kinds of displays

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -7,46 +7,59 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    
     @IBOutlet var redView: UIView!
     @IBOutlet var yellowView: UIView!
     @IBOutlet var greenView: UIView!
-    @IBOutlet var changeColorButton: UIButton!
+    
+    @IBOutlet var startButton: UIButton!
+    
+    private var currentLight = CurrentLight.red
+    private let lightIsOn: CGFloat = 1
+    private let lightIsOff: CGFloat = 0.3
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        redView.layer.cornerRadius = 50
-        yellowView.layer.cornerRadius = 50
-        greenView.layer.cornerRadius = 50
-        changeColorButton.layer.cornerRadius = 10
         
+        startButton.layer.cornerRadius = 10
+        
+        redView.alpha = lightIsOff
+        yellowView.alpha = lightIsOff
+        greenView.alpha = lightIsOff
     }
+    
+    override func viewWillLayoutSubviews() {
+        redView.layer.cornerRadius = redView.frame.width / 2
+        yellowView.layer.cornerRadius = yellowView.frame.width / 2
+        greenView.layer.cornerRadius = greenView.frame.width / 2
+    }
+    
+    @IBAction func startButtonTapped() {
+        if startButton.currentTitle == "START" {
+            startButton.setTitle("NEXT", for: .normal)
+        }
+        
+        switch currentLight {
+        case .red:
+            greenView.alpha = lightIsOff
+            redView.alpha = lightIsOn
+            currentLight = .yellow
+        case .yellow:
+            redView.alpha = lightIsOff
+            yellowView.alpha = lightIsOn
+            currentLight = .green
+        case .green:
+            greenView.alpha = lightIsOn
+            yellowView.alpha = lightIsOff
+            currentLight = .red
+        }
+    }
+}
 
-    @IBAction func changeColorButtonDidTapped() {
-        if yellowView.alpha < 1 && redView.alpha < 1 && greenView.alpha < 1 {
-            redView.alpha = 1
-        } else {
-            redView.alpha = 0.3
-        }
-        
-        if redView.alpha < 1 && greenView.alpha < 1 && yellowView.alpha < 1 {
-            yellowView.alpha = 1
-        } else {
-            yellowView.alpha = 0.3
-        }
-        
-        if yellowView.alpha < 1 && redView.alpha < 1 && greenView.alpha < 1 {
-            greenView.alpha = 1
-        } else {
-            greenView.alpha = 0.3
-        }
-        
-        if yellowView.alpha < 1 && greenView.alpha < 1 {
-            redView.alpha = 1
-        }
-        
-        changeColorButton.setTitle("Next", for: .normal)
+extension ViewController {
+    private enum CurrentLight {
+        case red, yellow, green
     }
 }
 
